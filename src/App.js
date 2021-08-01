069a7cbe885103b79d81fcd6d475e775eb448c62
ishitakeshawani <keshwaniishita054@gmail.com>
2021-08-01 23:32:24 +0530

    modified files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route} from 'react-router-dom';
 import './App.css';
+import Angle from './comps/Angle';
+import Area from './comps/Area';
+import Hypo from './comps/Hypo';
+import Quiz from './comps/Quiz';
+import Home from './comps/Home';
+import Header from "./comps/Header";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+      <Header />
+        <Route path="/" component={Home}   exact />
+        <Route path="/angle" component={Angle} />
+        <Route path="/area" component={Area} />
+        <Route path="/hypotenuse" component={Hypo} />
+        <Route path="/quiz" component={Quiz} />
+      </BrowserRouter>
     </div>
   );
 }
