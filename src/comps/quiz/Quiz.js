f481633578ef31f1d348464ae22b1ba0520cdee1
ishitakeshawani <keshwaniishita054@gmail.com>
2021-08-10 23:52:14 +0530

    added another 5 ques

--- src/comps/quiz/Quiz.js
+++ src/comps/quiz/Quiz.js
@@ -10,6 +10,9 @@ const Quiz = () => {
     5: "",
     6: "",
     7: "",
+    8: "",
+    9: "",
+    10: ""
   };
 
   const correctAnswers = {
@@ -18,8 +21,11 @@ const Quiz = () => {
     3: "yes",
     4: "yes",
     5: "yes",
-    // 6: "30",
-    // 7: "5"
+    6: "isosceles",
+    7: "30",
+    8: "both",
+    9: "5cm",
+    10: "scalene",
   };
   const [status, setStatus] = useState(initialUserAnswersState);
   const [correctans, setcorrectans] = useState(0)
@@ -117,13 +123,25 @@ const Quiz = () => {
           setStatus((prevState) => ({ ...prevState, 5: value }));
           break;
 
-        // case "QuestionSix":
-        //   setUserAnswers((prevState) => ({ ...prevState, 6: value }));
-        //   break;
+        case "q6":
+          setStatus((prevState) => ({ ...prevState, 6: value }));
+          break;
 
-        // case "QuestionSeven":
-        //   setUserAnswers((prevState) => ({ ...prevState, 7: value }));
-        //   break;
+        case "q7":
+          setStatus((prevState) => ({ ...prevState, 7: value }));
+          break;
+
+        case "q8":
+          setStatus((prevState) => ({ ...prevState, 8: value }));
+          break; 
+
+        case "q9":
+          setStatus((prevState) => ({ ...prevState, 9: value }));
+          break;
+
+        case "q10":
+          setStatus((prevState) => ({ ...prevState, 10: value }));
+          break;
 
         default:
           console.log("N/ot a Question");
@@ -150,7 +168,7 @@ const Quiz = () => {
           <form className="radioform" onSubmit={handleoutput}>
             <div className="ques ans">
               <div className="label-que">
-                1.If a triangle has angles 135<sup>0</sup>, 15<sup>0</sup>, 30
+                1. If a triangle has angles 135<sup>0</sup>, 15<sup>0</sup>, 30
                 <sup>0</sup>. Is it an obtuse triangle?
                 <br />
                 <label htmlFor="radio1" className="label">
@@ -173,8 +191,6 @@ const Quiz = () => {
                     name="q1"
                     checked={status[1] === "no"}
                     onChange={handleinput}
-                    // checked={resultarray.push("b")}
-
                     value="no"
                   />
                   no
@@ -183,7 +199,7 @@ const Quiz = () => {
             </div>
             <div className="ques ans">
               <div className="label-que">
-                2.If a triangle has angles 115<sup>0</sup>, 25<sup>0</sup>, 40
+                2. If a triangle has angles 115<sup>0</sup>, 25<sup>0</sup>, 40
                 <sup>0</sup>. Is it an acute triangle?
                 <br />
                 <label htmlFor="radio1" className="label">
@@ -214,7 +230,7 @@ const Quiz = () => {
             </div>
             <div className="ques ans">
               <div className="label-que">
-                3.If a triangle has angles 90<sup>0</sup>, 60<sup>0</sup>, 30
+                3. If a triangle has angles 90<sup>0</sup>, 60<sup>0</sup>, 30
                 <sup>0</sup>. Is it a right angle triangle?
                 <br />
                 <label htmlFor="radio1" className="label">
@@ -245,7 +261,7 @@ const Quiz = () => {
             </div>
             <div className="ques ans">
               <div className="label-que">
-                4.A triangle has angles 60<sup>0</sup>, 60<sup>0</sup>, 60
+                4. A triangle has angles 60<sup>0</sup>, 60<sup>0</sup>, 60
                 <sup>0</sup>. Is it an equilateral triangle?
                 <br />
                 <label htmlFor="radio1" className="label">
@@ -276,7 +292,7 @@ const Quiz = () => {
             </div>
             <div className="ques ans">
               <div className="label-que">
-                If a triangle has angles 25<sup>0</sup>, 75<sup>0</sup>, 80
+                5. If a triangle has angles 25<sup>0</sup>, 75<sup>0</sup>, 80
                 <sup>0</sup>. Is it an acute triangle?
                 <br />
                 <label htmlFor="radio1" className="label">
@@ -302,6 +318,221 @@ const Quiz = () => {
                     value="no"
                   />
                   no
+                </label>
+              </div>
+            </div>
+            <div className="ques ans">
+              <div className="label-que">
+                6. If a triangle has 2 sides with equal lengths and 75
+                <sup>0</sup> angle between them. What is the type of triangle?
+                <br />
+                <label htmlFor="radio1" className="label">
+                  <input
+                    required
+                    type="radio"
+                    id="radio1"
+                    name="q6"
+                    checked={status[6] === "equilateral"}
+                    onChange={handleinput}
+                    value="equilateral"
+                  />
+                  equilateral
+                </label>
+                <label htmlFor="radio1" className="label">
+                  <input
+                    required
+                    type="radio"
+                    id="radio1"
+                    name="q6"
+                    checked={status[6] === "isosceles"}
+                    onChange={handleinput}
+                    value="isosceles"
+                  />
+                  isosceles
+                </label>
+                <label htmlFor="radio1" className="label">
+                  <input
+                    required
+                    type="radio"
+                    id="radio1"
+                    name="q6"
+                    checked={status[6] === "right angle traingle"}
+                    onChange={handleinput}
+                    value="right angle traingle"
+                  />
+                  right angle traingle
+                </label>
+              </div>
+            </div>
+            <div className="ques ans">
+              <div className="label-que">
+                7. If a triangle has 2 angles of 75<sup>0</sup>. What is the
+                measure of third angle in degree?
+                <br />
+                <label htmlFor="radio1" className="label">
+                  <input
+                    required
+                    type="radio"
+                    id="radio1"
+                    name="q7"
+                    checked={status[7] === "25"}
+                    onChange={handleinput}
+                    value="25"
+                  />
+                  25
+                </label>
+                <label htmlFor="radio1" className="label">
+                  <input
+                    required
+                    type="radio"
+                    id="radio1"
+                    name="q7"
+                    checked={status[7] === "30"}
+                    onChange={handleinput}
+                    value="30"
+                  />
+                  30
+                </label>
+                <label htmlFor="radio1" className="label">
+                  <input
+                    required
+                    type="radio"
+                    id="radio1"
+                    name="q7"
+                    checked={status[7] === "15"}
+                    onChange={handleinput}
+                    value="15"
+                  />
+                  15
+                </label>
+              </div>
+            </div>
+            <div className="ques ans">
+              <div className="label-que">
+                8. If a triangle has 2 sides with equal lengths and 60
+                <sup>0</sup> angle between them. What is the type of triangle?
+                <br />
+                <label htmlFor="radio1" className="label">
+                  <input
+                    required
+                    type="radio"
+                    id="radio1"
+                    name="q8"
+                    checked={status[8] === "equilateral"}
+                    onChange={handleinput}
+                    value="equilateral"
+                  />
+                  equilateral
+                </label>
+                <label htmlFor="radio1" className="label">
+                  <input
+                    required
+                    type="radio"
+                    id="radio1"
+                    name="q8"
+                    checked={status[8] === "isosceles"}
+                    onChange={handleinput}
+                    value="isosceles"
+                  />
+                  isosceles
+                </label>
+                <label htmlFor="radio1" className="label">
+                  <input
+                    required
+                    type="radio"
+                    id="radio1"
+                    name="q8"
+                    checked={status[8] === "both"}
+                    onChange={handleinput}
+                    value="both"
+                  />
+                  both
+                </label>
+              </div>
+            </div>
+            <div className="ques ans">
+              <div className="label-que">
+                9. The perimeter of an equilateral triangle is 15cm. What is the
+                length of one side?
+                <br />
+                <label htmlFor="radio1" className="label">
+                  <input
+                    required
+                    type="radio"
+                    id="radio1"
+                    name="q9"
+                    checked={status[9] === "15cm"}
+                    onChange={handleinput}
+                    value="15cm"
+                  />
+                  15cm
+                </label>
+                <label htmlFor="radio1" className="label">
+                  <input
+                    required
+                    type="radio"
+                    id="radio1"
+                    name="q9"
+                    checked={status[9] === "45cm"}
+                    onChange={handleinput}
+                    value="45cm"
+                  />
+                  45cm
+                </label>
+                <label htmlFor="radio1" className="label">
+                  <input
+                    required
+                    type="radio"
+                    id="radio1"
+                    name="q9"
+                    checked={status[9] === "5cm"}
+                    onChange={handleinput}
+                    value="5cm"
+                  />
+                  5cm
+                </label>
+              </div>
+            </div>
+            <div className="ques ans">
+              <div className="label-que">
+                10. If a triangle has sides of 2cm, 3cm, 4cm, what is the type
+                of triangle?
+                <br />
+                <label htmlFor="radio1" className="label">
+                  <input
+                    required
+                    type="radio"
+                    id="radio1"
+                    name="q10"
+                    checked={status[10] === "equilateral"}
+                    onChange={handleinput}
+                    value="equilateral"
+                  />
+                  equilateral
+                </label>
+                <label htmlFor="radio1" className="label">
+                  <input
+                    required
+                    type="radio"
+                    id="radio1"
+                    name="q10"
+                    checked={status[10] === "isosceles"}
+                    onChange={handleinput}
+                    value="isosceles"
+                  />
+                  isosceles
+                </label>
+                <label htmlFor="radio1" className="label">
+                  <input
+                    required
+                    type="radio"
+                    id="radio1"
+                    name="q10"
+                    checked={status[10] === "scalene"}
+                    onChange={handleinput}
+                    value="scalene"
+                  />
+                  scalene
                 </label>
               </div>
             </div>
