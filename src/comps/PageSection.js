68630dfc2c281650807c5205ac1434866d7c327f
ishitakeshawani <keshwaniishita054@gmail.com>
2021-08-11 13:32:03 +0530

    changed the description of pagesection

--- src/comps/PageSection.js
+++ src/comps/PageSection.js
@@ -3,19 +3,20 @@ import { ReactComponent as Poster } from './../poster.svg';
 
 const PageSection = () => {
     return (
-        <div className="page">
-            <div className="page-col">
-            <h2>Welcome to Fun with <span>Triangles</span></h2>
-            <p>
-                Here you can find angles of triangle,Area of triangle. <br></br>
-                Hypotenuse of Traingle and quiz for triangles <br></br>
-                Are you excited for it?Share it with others..
-            </p>
-            </div>
-            <Poster className="top-right-i" height="60%" width="60%" />
-           
-            
+      <div className="page">
+        <div className="page-col">
+          <h2>
+            Welcome to Fun with <span>Triangles</span>
+          </h2>
+          <p>
+            Here you can find angles of the triangle, Area of the triangle.{" "}
+            <br></br>
+            Hypotenuse of Triangle and quiz for triangles <br></br>
+            Are you excited about it? Share it with others...
+          </p>
         </div>
+        <Poster className="top-right-i" height="60%" width="60%" />
+      </div>
     );
 }
 
